acfa68ae7d25d114d35dd06e8f2bb43f309f69ad
Anushka kakeriwal <123049752+anushkaWeb@users.noreply.github.com>
2023-01-25 12:38:48 +0530

    Add files via upload

---
+++ menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakode",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo Pakora is a very popular Indian snack where thinly sliced potatoes are coated with a lightly spiced chickpea flour batter and deep-fried until golden and crispy. These are called Aloo Bajji or Potato Bajii in South India. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Corn is a cereal grain. Sweet corn is a flavorful and nutritious variety of corn that is seasonal during summer. Sweet corn is packed with vitamins (such as vitamin A, vitamin C, and B vitamins) as well as phytonutrients like . ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chole Bhature or Chana Bhatura is one of the most popular, tempting and flavorful dishes from Punjabi Cuisine. The union of Chickpea Curry and Fried Flatbreads is known as Chole Bhature.  ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Hot, cheesy pizza loaded with your favorite veggies is one of the most fun and easy dinners to make at home. With just a few key tips you can create a restaurant-quality crust, and treat yourself to the best homemade pizza  ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "You may eat at the fanciest restaurant or the most elaborate buffet, but the appeal of a simple veg thali is unbeatable. At the end of the day what you really need is simple and robust home cooked food. ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Sheera is one of the most popular Indian recipes offered to God in the form of naivedyam in temples and on special occasions like Diwali, Navratri, Ganesh Chaturthi, and Varalakshmi vratham.  ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "This is a Punjabi household favourite. Growing up in my parents home, this dish was often on the menu for our much anticipated Saturday lunch. When Rajma Chawal was prepared at our school cafe. ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "With some nice spicy green chutney, sweet tamarind chutney and those fried green chilies on the side, yummmmm! And, do not forget the garama garam (steaming hot) masala chai to go along. ",
+    },
+  ];
+  
+  export default Menu;
